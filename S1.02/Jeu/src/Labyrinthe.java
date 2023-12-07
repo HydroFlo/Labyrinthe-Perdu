d4ec4580fa90e5bf31ca9a963a5e75e749a4adb8
HydroFlo <HydroGamer@outlook.fr>
2023-12-07 09:03:22 +0100

    Squellette

--- S1.02/Jeu/src/Labyrinthe.java
+++ S1.02/Jeu/src/Labyrinthe.java
@@ -32,9 +32,35 @@ class Labyrinthe extends Program{
         return res;
     }
 
-    int[] grille() 72 x 19
+    char ControleSaisie(){ // verifie que l'utilisateur saisisse bien 1 caractere
+        do{
+            String choix = readString();
+        }while(length(choix)== 1);
+        return charAt(choix, 0);
+
+
+    }
+
+    char[][] genererLab(int nbSalle){ // genere un Layrinthe de nbSalle salle et d'une taille de 72 x 19 par salle
+        return new char[1][1];
+    } 
+
+    void deplacement(char[][] Lab, char direction){ //vérifie si déplacement possible, si oui l'effectue
+
+    }
+
+    void afficheLab(char[] Lab){ //affiche le Labyrinthe (@ = mur, P = perso, E = sortie, M = monstre, B = boss, S = shop)
+
+    }
 
     void algorithm(){
+        genererLab(int nbCase) //genere le Layrinthe
         print(readFile("ressources/img/Presentation.txt"));
+        while(!jeufini()){
+            char choix = ControleSaisie();
+            if(choix == 'z' || choix == 'q' || choix == 's' || choix == 'd'){
+                deplacement(jeu, choix);
+            }
+        }
     }
 }
